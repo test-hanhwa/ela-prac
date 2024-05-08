1535714d99f3aa859f4af1122bc650ac36d065af
moomint8 <office05053@gmail.com>
2024-05-08 10:41:11 +0900

    setting

---
+++ src/main/java/com/ohgiraffers/elaprac/ElaPracApplication.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.elaprac;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ElaPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElaPracApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {return new ModelMapper();}
+
+}
